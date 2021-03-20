84edba1956a5b511940f4fb34f5e52d1640922f7
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-03-20 17:56:00 -0300

    feat: add the first models to core app
    add Author model and Book model.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField('Name', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField('Name', max_legth=200)
+    edition = models.IntegerField()
+    publication_year = models.DateField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
