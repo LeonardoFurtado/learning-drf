2dda67b53fac58f4de71dc187935e3ff6fd9a5ee
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-03-21 19:09:45 -0300

    feat: add verbose names to models.

--- core/models.py
+++ core/models.py
@@ -7,6 +7,10 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'author'
+        verbose_name_plural = 'authors'
+
 
 class Book(models.Model):
     name = models.CharField('Name', max_length=200)
@@ -16,3 +20,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
