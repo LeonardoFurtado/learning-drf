6e8b3754403953aaaeebda44d42c2d5e17a81e70
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-03-20 23:52:26 -0300

    feat: create initial APIViews.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Author, Book
+from .serializers import BookSerializer, AuthorSerializer
+
+
+class AuthorAPIView(APIView):
+    """
+    Author API
+    """
+
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AuthorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class BookAPIView(APIView):
+    """
+    Book API
+    """
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
